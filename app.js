544c8884d0565df6fb184b86bd4e2ca0bdad0a8a
believe <believeosawaru2@gmail.com>
2024-08-01 15:19:19 +0100

    nm

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const app = express();
 
 connectToDb();
 
-app.use(cors);
+// app.use(cors({
+//   origin: 'http://127.0.0.1:5500'
+// }));
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/api/v1/auth", authRoutes);
