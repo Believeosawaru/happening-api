2afdbc5ddc294a66d4c565577c79ee22dfdf30f3
believe <believeosawaru2@gmail.com>
2024-08-01 14:58:56 +0100

    new

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRoutes from "./routes/index.js";
 import connectToDb from "./init/mongodb.js";
 import errorHandler from "./middlewares/errorHandler.js"
+import cors from "cors";
 const app = express();
 
 connectToDb();
 
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/api/v1/auth", authRoutes);
