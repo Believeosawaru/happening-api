bfde565c4b555d690d1bdcccc9edb71f899b38d7
believe <believeosawaru2@gmail.com>
2024-08-08 00:18:42 +0100

    ds5rdtuyhvj

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -16,10 +16,10 @@ const isAuth = async (req, res, next) => {
         
         jwt.verify(token, jwtSecret, (err, user) => {
             if (err) {
-                return res.status(401).json({
+                return res.status(403).json({
                     code: 403,
                     status: false, 
-                    message: "Fotbidden Request"
+                    message: err.message
                 })
             }
 
