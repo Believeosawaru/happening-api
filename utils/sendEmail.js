2bda7810250e64ab59c70ecf5b7a31071a912aca
believe <believeosawaru2@gmail.com>
2024-08-07 09:02:05 +0100

    dfwvyevucjecwec

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,30 +1,31 @@
 import nodemailer from "nodemailer";
 import { senderEmail, emailPass } from "../config/keys.js";
 
-const sendEmail = async ({from, emailTo, subject, code, content}) => {
+const sendEmail = async ({ emailTo, subject, code, content }) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         secure: false,
         auth: {
-            user: senderEmail,
-            pass: emailPass
-        }
-    });
-
-    const message  = {
-        from,
-        to: emailTo,
-        subject,
-        html: `
-        <div>
-          <h3>Use The Code Below To ${content}</h3>
-          <p><strong>Code: ${code}</strong></p>
-        </div>
-        `
-    }
-
-    await transporter.sendMail(message);
-}
+          user: senderEmail,
+          pass: emailPass
+        },
+      });
+  
+    const message = {
+      to: emailTo,
+      subject,
+      html: `
+              <div>
+                <h3>Your 6-digit Code to ${content}</h3>
+                <p>Code: ${code}</p>
+              </div>
+          `,
+    };
+  
+    transporter.sendMail(message);
+  
+    console.log("sent successfully");
+  };
 
 export default sendEmail;
