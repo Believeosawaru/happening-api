774e73d7d7322446aaa334686a592f8034baf7b3
believe <believeosawaru2@gmail.com>
2024-08-12 12:51:21 +0100

    0iuytreyhtj

--- controllers/user.js
+++ controllers/user.js
@@ -194,16 +194,16 @@ const searchUsers = async (req, res, next) => {
             throw new Error("No Search Query Provided");
         }
 
-        const users = await User.find({
-            $or: [{firstName: {$regex: query, $options: "i"}}, {lastName: {$regex: query, $options: "i"}},
-            {email: {$regex: query, $options: "i"}}
-            ]
-        }).select("firstName lastName email")
+        // const users = await User.find({
+        //     $or: [{firstName: {$regex: query, $options: "i"}}, {lastName: {$regex: query, $options: "i"}},
+        //     {email: {$regex: query, $options: "i"}}
+        //     ]
+        // }).select("firstName lastName email")
 
         res.status(200).json({
             code: 200,
             status: true,
-            users
+            query
         })
     } catch (error) {
         next(error);
