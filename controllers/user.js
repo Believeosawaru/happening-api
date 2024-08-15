58e3f663f8a1943a245f29de4278a03407b67e9a
believe <believeosawaru2@gmail.com>
2024-08-15 14:23:36 +0100

    nvewuvch feh iv

--- controllers/user.js
+++ controllers/user.js
@@ -405,24 +405,24 @@ const leaveGroup = async (req, res, next) => {
             })
         }
 
-        // const isMember = group.members.some(member => member.user.toString() === req.user._id.toString());
+        const isMember = group.members.some(member => member.user.toString() === req.user._id.toString());
 
-        // if (!isMember) {
-        //     res.status(400).json({
-        //         code: 400,
-        //         status: false,
-        //         message: "You Are Not A Member Of This Group"
-        //     });
-        // }
+        if (!isMember) {
+            res.status(400).json({
+                code: 400,
+                status: false,
+                message: "You Are Not A Member Of This Group"
+            });
+        }
 
-        // group.members = group.members.filter(member => member.user.toString() !== req.user._id.toString());
+        group.members = group.members.filter(member => member.user.toString() !== req.user._id.toString());
 
-        // await group.save();
+        await group.save();
 
         res.status(200).json({
             code: 200,
             status: true,
-            message: req.user._id
+            message: "You've Left This Group"
         });
     } catch (error) {
         next(error)
