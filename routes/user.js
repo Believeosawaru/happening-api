e6ed857c8c5704c50363e4874bcc81c330692e54
believe <believeosawaru2@gmail.com>
2024-10-12 19:34:32 +0100

    BHJ H KNN

--- routes/user.js
+++ routes/user.js
@@ -10,60 +10,60 @@ const userRoutes = express.Router();
 
 // userRoutes.get("/home", isAuth, homeController);
 
-userRoutes.get("/user-profile", isAuth, userProfile)
+// userRoutes.get("/user-profile", isAuth, userProfile)
 
-userRoutes.get("/home-groups", isAuth, latestGroup);
+// userRoutes.get("/home-groups", isAuth, latestGroup);
 
-userRoutes.get("/all-groups", isAuth, allGroups);
+// userRoutes.get("/all-groups", isAuth, allGroups);
 
-userRoutes.post("/create-group", isAuth, groupController);
+// userRoutes.post("/create-group", isAuth, groupController);
 
-userRoutes.get("/groups", isAuth, displayGroupController);
+// userRoutes.get("/groups", isAuth, displayGroupController);
 
-userRoutes.get("/group/:groupId", isAuth, groupInfo);
+// userRoutes.get("/group/:groupId", isAuth, groupInfo);
 
-userRoutes.post("/group/:groupId/generate-link", isAuth, isGroupCreator, generateLink);
+// userRoutes.post("/group/:groupId/generate-link", isAuth, isGroupCreator, generateLink);
 
-userRoutes.post("/join-group/:groupId", isAuth, joinGroup);
+// userRoutes.post("/join-group/:groupId", isAuth, joinGroup);
 
-userRoutes.post("/leave-group/:groupId", isAuth, leaveGroup);
+// userRoutes.post("/leave-group/:groupId", isAuth, leaveGroup);
 
-userRoutes.post("/join-group/:groupToken/invite-link", isAuth, joinViaLink);
+// userRoutes.post("/join-group/:groupToken/invite-link", isAuth, joinViaLink);
 
-userRoutes.put("/edit-group-info/:groupId", isAuth, isGroupCreator, editGroupInfo);
+// userRoutes.put("/edit-group-info/:groupId", isAuth, isGroupCreator, editGroupInfo);
 
-userRoutes.get("/group-details/:groupId", isAuth, isGroupCreator, showGroupInfo);
+// userRoutes.get("/group-details/:groupId", isAuth, isGroupCreator, showGroupInfo);
 
-userRoutes.delete("/delete-group/:groupId", isAuth, isGroupCreator, deleteGroup);
+// userRoutes.delete("/delete-group/:groupId", isAuth, isGroupCreator, deleteGroup);
 
-userRoutes.get("/search-users/:groupId", isAuth, isGroupCreator,  searchUsers);
+// userRoutes.get("/search-users/:groupId", isAuth, isGroupCreator,  searchUsers);
 
-userRoutes.post("/group/:groupId/add-member", isAuth, isGroupCreator, addUser);
+// userRoutes.post("/group/:groupId/add-member", isAuth, isGroupCreator, addUser);
 
-userRoutes.get("/group/search/email-users/:groupId", isAuth, isGroupCreator, searchUsersEmail);
+// userRoutes.get("/group/search/email-users/:groupId", isAuth, isGroupCreator, searchUsersEmail);
 
-userRoutes.post("/group/:groupId/send-invite", isAuth, isGroupCreator, sendGroupLink);
+// userRoutes.post("/group/:groupId/send-invite", isAuth, isGroupCreator, sendGroupLink);
 
-userRoutes.post("/create-event", isAuth, eventController);
+// userRoutes.post("/create-event", isAuth, eventController);
 
-userRoutes.get("/events", isAuth, displayEventController);
+// userRoutes.get("/events", isAuth, displayEventController);
 
-userRoutes.get("/event/:eventId", isAuth, eventInfo);
+// userRoutes.get("/event/:eventId", isAuth, eventInfo);
 
-userRoutes.put("/edit-event-info/:eventId", isAuth, isEventCreator, editEventInfo);
+// userRoutes.put("/edit-event-info/:eventId", isAuth, isEventCreator, editEventInfo);
 
-userRoutes.get("/event-details/:eventId", isAuth, isEventCreator, showEventInfo);
+// userRoutes.get("/event-details/:eventId", isAuth, isEventCreator, showEventInfo);
 
-userRoutes.delete("/delete-event/:eventId", isAuth, isEventCreator, deleteEvent);
+// userRoutes.delete("/delete-event/:eventId", isAuth, isEventCreator, deleteEvent);
 
-userRoutes.get("/all-events", isAuth, allEvents);
+// userRoutes.get("/all-events", isAuth, allEvents);
 
-userRoutes.get("/home-events", isAuth, latestEvent);
+// userRoutes.get("/home-events", isAuth, latestEvent);
 
-userRoutes.get("/event/search-users/:eventId", isAuth, isEventCreator, searchUserEvent);
+// userRoutes.get("/event/search-users/:eventId", isAuth, isEventCreator, searchUserEvent);
 
-userRoutes.post("/event/:eventId/send-invite", isAuth, isEventCreator, sendEventIv);
+// userRoutes.post("/event/:eventId/send-invite", isAuth, isEventCreator, sendEventIv);
 
-userRoutes.post("/join-event/:eventId", isAuth, eventJoin);
+// userRoutes.post("/join-event/:eventId", isAuth, eventJoin);
 
 export default userRoutes;
