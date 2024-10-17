13425430e7553137b6ee2137bb04b40a8d20c939
believe <believeosawaru2@gmail.com>
2024-10-18 00:33:47 +0100

    cbnkj

--- config/keys.js
+++ config/keys.js
@@ -6,5 +6,6 @@ const connectionUrl = process.env.CONNECTION_URL;
 const jwtSecret = process.env.JWT_SECRET;
 const senderEmail = process.env.SENDER_EMAIL;
 const emailPass = process.env.EMAIL_PASS
+const openAIKey = process.env.OPENAI_API_KEY;
 
-export {port, connectionUrl, jwtSecret, senderEmail, emailPass};
+export {port, connectionUrl, jwtSecret, senderEmail, emailPass, openAIKey};
