f6087132569c5a93964cbd818f5b8541fd2a615e
believe <believeosawaru2@gmail.com>
2024-10-18 00:56:49 +0100

    j kf nmcf

--- utils/assignCategory.js
+++ utils/assignCategory.js
@@ -1,8 +1,6 @@
 import fetch from "node-fetch";
 
-// import { openAIKey } from "../config/keys.js";
-
-const openAIKey = "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";
+import { openAIKey } from "../config/keys.js";
 
 async function assignCategory(name, description) {
     const prompt = `Based on the following title and description, assign one of the following categories:\n\n` +
@@ -16,7 +14,7 @@ async function assignCategory(name, description) {
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer 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`,
+            'Authorization': `Bearer ${openAIKey}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
