e864dd64e9de4dbb93f5dc959f953ffd2c76b14d
believe <believeosawaru2@gmail.com>
2024-10-18 00:52:29 +0100

    j cv,nc ,c

--- utils/assignCategory.js
+++ utils/assignCategory.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 // import { openAIKey } from "../config/keys.js";
 
-const openAIKey = "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"
+const openAIKey = "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";
 
 async function assignCategory(name, description) {
     const prompt = `Based on the following title and description, assign one of the following categories:\n\n` +
