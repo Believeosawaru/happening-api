bfe45859500de79c664d310cb352b66a457e114b
believe <believeosawaru2@gmail.com>
2024-10-28 22:17:17 +0100

    nb jkhnm

--- routes/user.js
+++ routes/user.js
@@ -78,12 +78,12 @@ userRoutes.post("/join-event/:eventId", isAuth, eventJoin);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './routes/'); // Specify the directory where files should be stored
+      cb(null, './uploads/'); // Specify the directory where files should be stored
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + '-' + file.originalname); // Specify the filename
     }
-  });
+});
  
 const upload = multer({ storage: storage });
 
