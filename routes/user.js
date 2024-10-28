2a8ab197bc06a76681553e6aa945c5c22ea43ce5
believe <believeosawaru2@gmail.com>
2024-10-28 13:55:16 +0100

    nj knmv

--- routes/user.js
+++ routes/user.js
@@ -76,7 +76,7 @@ userRoutes.post("/event/:eventId/send-invite", isAuth, isEventCreator, sendEvent
 
 userRoutes.post("/join-event/:eventId", isAuth, eventJoin);
 
-userRoutes.post('/upload', uploadImage);
+userRoutes.post('/upload', isAuth, uploadImage);
 
 // userRoutes.get('/images/:filename', getImage);
 
