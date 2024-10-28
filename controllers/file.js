1d7e0d2f9948b281bd9294a337b4d641792f98b3
believe <believeosawaru2@gmail.com>
2024-10-28 13:35:39 +0100

    jihnbuhkjnm

--- controllers/file.js
+++ controllers/file.js
@@ -2,35 +2,29 @@ import User from "../models/User.js";
 
 const uploadImage = async (req, res, next) => {
    try {
-      // const userId = req.user._id;
+      const userId = req.user._id;
 
-      // const user = await User.findById(userId);
+      const user = await User.findById(userId);
 
-      res.status(200).json({
-        code: 200,
-        status: true,
-        message: "Working"
-      })
+      if (!req.file) {
+        res.status(400).json({
+          code: 400,
+          status: false,
+          message: "No file uploaded"
+        });
+      }
 
-      // if (!req.file) {
-      //   res.status(400).json({
-      //     code: 400,
-      //     status: false,
-      //     message: "No file uploaded"
-      //   });
+        const filePath = `/upload/${req.file.filename}`;
 
-      //   const filePath = `/upload/${req.file.filename}`;
+        // user.profilePicture = filePath;
 
-      //   // user.profilePicture = filePath;
+        // user.save();
 
-      //   // user.save();
-
-      //   res.status(200).json({
-      //     code: 200,
-      //     status: true,
-      //     message: "Image Uploaded Successfully"
-      //   })
-      // }
+        res.status(200).json({
+          code: 200,
+          status: true,
+          message: "Image Uploaded Successfully"
+        });
    } catch (error) {
         next(error)
    }
