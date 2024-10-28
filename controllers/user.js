3bcc2d2f41f2616fcd9effc52b4e593fd5ec1d68
believe <believeosawaru2@gmail.com>
2024-10-28 21:47:36 +0100

    mjnijbknm

--- controllers/user.js
+++ controllers/user.js
@@ -300,7 +300,7 @@ const uploadPicture = async (req, res, next) => {
         res.status(200).json({
             code: 200,
             status: true,
-            data: "work"
+            data: `path: ${req.file}`
         });
     } catch (error) {
         next(error);
