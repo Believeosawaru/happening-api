7ca613a69f97e546dd4c0f6af1034ebe6422be0b
believe <believeosawaru2@gmail.com>
2024-10-28 14:09:13 +0100

    cvn jknm

--- controllers/file.js
+++ controllers/file.js
@@ -1,11 +1,29 @@
-// import User from "../models/User.js";
+import User from "../models/User.js";
 
 const uploadImage = async (req, res, next) => {
    try {
+      const userId = req.user._id;
+
+      const user = await User.findById(userId);
+
+      if (!req.file) {
+        res.status(400).json({
+          code: 400,
+          status: false,
+          message: "No file uploaded"
+        });
+      }
+
+      const filePath = `/upload/${req.file.filename}`;
+
+        // user.profilePicture = filePath;
+
+        // user.save();
+
         res.status(200).json({
-            code: 200,
-            status: true,
-            message: "DONE"
+          code: 200,
+          status: true,
+          message: "Image Uploaded Successfully"
         });
    } catch (error) {
         next(error)
