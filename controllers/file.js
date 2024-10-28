a0fb07aa1342d5334a5494ed172177694344f2da
believe <believeosawaru2@gmail.com>
2024-10-28 14:07:25 +0100

    vnj kmn

--- controllers/file.js
+++ controllers/file.js
@@ -2,28 +2,10 @@
 
 const uploadImage = async (req, res, next) => {
    try {
-      // const userId = req.user._id;s
-
-      // const user = await User.findById(userId);
-
-      // if (!req.file) {
-      //   res.status(400).json({
-      //     code: 400,
-      //     status: false,
-      //     message: "No file uploaded"
-      //   });
-      // }
-
-      // const filePath = `/upload/${req.file.filename}`;
-
-        // user.profilePicture = filePath;
-
-        // user.save();
-
         res.status(200).json({
             code: 200,
             status: true,
-            message: "Image Uploaded Successfully"
+            message: "DONE"
         });
    } catch (error) {
         next(error)
