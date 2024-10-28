788352a090c23a3fef895d2c3707dfa4a9c63cc7
believe <believeosawaru2@gmail.com>
2024-10-28 19:17:59 +0100

    ibuh jnbm

--- routes/user.js
+++ routes/user.js
@@ -76,11 +76,7 @@ userRoutes.post("/event/:eventId/send-invite", isAuth, isEventCreator, sendEvent
 
 userRoutes.post("/join-event/:eventId", isAuth, eventJoin);
 
-userRoutes.post("/upload", isAuth, (req, res, next) => {
-    res.status(200).json({
-        message: "wok"
-    })
-});
+userRoutes.post("/upload", isAuth, homeController);
 
 // userRoutes.post("/upload", upload.single('profilePicture'), uploadImage);
 
