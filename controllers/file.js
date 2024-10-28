fb708fdb3ecae72fad5865a38731f4e3816aa195
believe <believeosawaru2@gmail.com>
2024-10-28 12:45:24 +0100

    fvubifhfnm

--- controllers/file.js
+++ controllers/file.js
@@ -5,56 +5,49 @@ import Grid from "gridfs-stream";
 import { connectionUrl } from "../config/keys.js";
 import User from "../models/User.js";
 
-// // const storage = new GridFsStorage({
-// //     url: connectionUrl, 
-// //     file: (req, file) => {
-// //       return {
-// //         filename: file.originalname,
-// //         bucketName: 'uploads' // Optional: specify a bucket name
-// //       };
-// //     }
-// // });
-
-// const upload = multer({ storage });
-
-// // Upload image
-// const uploadImage = async (req, res) => {
-//    try {
-//         const userId = req.user._id; 
-//         const uploadedFile = req.file;
-
-//         if (!uploadedFile) {
-//             res.status(400).json({ 
-//                 code: 400,
-//                 status: false,
-//                 message: 'File not uploaded' 
-//             });
-//         }
-
-//         await User.findByIdAndUpdate(userId, {
-//         profilePicture: uploadedFile.filename
-//         });
-
-//         res.status(200).json({ 
-//             code: 200,
-//             status: true,
-//             file: uploadedFile
-//          });
-//    } catch (error) {
-//         next(error)
-//    }
-// };
-
-// // Get image by filename
-// const getImage = (req, res) => {
-//   const gfs = Grid(mongoose.connection.db, mongoose.mongo);
-//   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-//     if (!file || file.length === 0) {
-//       return res.status(404).json({ err: 'No file exists' });
+// const storage = new GridFsStorage({
+//     url: connectionUrl, 
+//     file: (req, file) => {
+//       return {
+//         filename: file.originalname,
+//         bucketName: 'uploads' // Optional: specify a bucket name
+//       };
 //     }
-//     const readstream = gfs.createReadStream(file.filename);
-//     readstream.pipe(res);
-//   });
-// };
+// });
 
-// export { upload, uploadImage, getImage }
+const upload = multer({ storage });
+
+// Upload image
+const uploadImage = async (req, res) => {
+   try {
+        // const userId = req.user._id; 
+        // const uploadedFile = req.file;
+
+        // if (!uploadedFile) {
+        //     res.status(400).json({ 
+        //         code: 400,
+        //         status: false,
+        //         message: 'File not uploaded' 
+        //     });
+        // }
+
+        // await User.findByIdAndUpdate(userId, {
+        // profilePicture: uploadedFile.filename
+        // });
+
+        // res.status(200).json({ 
+        //     code: 200,
+        //     status: true,
+        //     file: uploadedFile
+        //  });
+   } catch (error) {
+        next(error)
+   }
+};
+
+// Get image by filename
+const getImage = (req, res) => {
+  
+};
+
+export { upload, uploadImage, getImage }
