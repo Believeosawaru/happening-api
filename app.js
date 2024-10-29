13c0fd9f31adb63870147359c7819e1a86bba009
believe <believeosawaru2@gmail.com>
2024-10-29 14:14:20 +0100

    jjnibjlknm

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const __dirname = dirname(__filename);
 connectToDb();
 
 app.set("view-engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use('/routes', express.static('routes'));
+app.set("views", path.join(__dirname, "views")); 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // app.use(cors({
 //   origin: 'http://127.0.0.1:5500'
