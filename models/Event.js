bd95dd3478cc4bacd0061ba049cb0d3ef20cf471
believe <believeosawaru2@gmail.com>
2024-11-07 14:37:46 +0100

    cvhbh jgn

--- models/Event.js
+++ models/Event.js
@@ -27,11 +27,12 @@ const slugify = (text) => {
 eventSchema.pre("save", async function (next) {
     if (this.isNew || this.isModified("name")) {
         if (this.name) {
-            let newSlug = slugify(this.name);
-            let count = 1;
+            // let newSlug = slugify(this.name);
+            // let count = 1;
 
             while (await mongoose.model("event").findOne({slug: newSlug})) {
-                newSlug = `${slugify(this.name)}-${count++}`;
+                newSlug = `${this.name}`;
+                // newSlug = `${slugify(this.name)}-${count++}`;
             }
 
             this.slug = newSlug;
