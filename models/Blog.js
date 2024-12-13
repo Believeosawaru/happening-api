96027c20a4c841b97c4077207602cea9574f34b1
believe <believeosawaru2@gmail.com>
2024-12-13 13:20:27 +0100

    mjnbljhkn

--- models/Blog.js
+++ models/Blog.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const blogSchema = new Schema({
     content: { type: String, required: true }, 
     author: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-    tags: [String],
     createdAt: { type: Date, default: Date.now }, 
     updatedAt: { type: Date }
 }, {timestamps: true});
