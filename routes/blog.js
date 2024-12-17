5bf54900de822337439e1231a3f1d875ea258e6a
believe <believeosawaru2@gmail.com>
2024-12-17 09:34:28 +0100

    fbuikjf

--- routes/blog.js
+++ routes/blog.js
@@ -6,7 +6,7 @@ import isAdmin from "../middlewares/isAdmin.js";
 const blogRoutes = express.Router();
 
 blogRoutes.post("/create-post", isAuth, isAdmin, createPost);
-blogRoutes.post("/load-posts", isAuth, loadPosts);
+blogRoutes.get("/load-posts", isAuth, loadPosts);
 blogRoutes.post("/edit-post/:id", isAuth, isAdmin, editPost);
 blogRoutes.delete("/delete-post/:id", isAuth, isAdmin, deletePost);
 
