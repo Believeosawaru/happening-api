8acd30146974348e3ea3d9ff324ece3a1c7bbfc8
believe <believeosawaru2@gmail.com>
2024-12-17 12:40:03 +0100

    fjnb jknm f

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,12 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { createPost, deletePost, editPost, loadPosts } from "../controllers/index.js";
+import { createPost, deletePost, editPost, loadCurrentPost, loadPosts } from "../controllers/index.js";
 import isAdmin from "../middlewares/isAdmin.js";
 
 const blogRoutes = express.Router();
 
 blogRoutes.post("/create-post", isAuth, isAdmin, createPost);
+blogRoutes.get("/load-current-post/:id", isAuth, isAdmin, loadCurrentPost);
 blogRoutes.get("/load-posts", isAuth, loadPosts);
 blogRoutes.post("/edit-post/:id", isAuth, isAdmin, editPost);
 blogRoutes.delete("/delete-post/:id", isAuth, isAdmin, deletePost);
