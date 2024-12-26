0d79a6b82cfcdb7944f81ea711b592251f29613f
believe <believeosawaru2@gmail.com>
2024-12-27 00:43:58 +0100

    nnb hjknm

--- controllers/blog.js
+++ controllers/blog.js
@@ -6,9 +6,8 @@ const createPost = async (req, res, next) => {
         const { content } = req.body;
         const userId = new ObjectId(String(req.user._id));
         const file = req.file;
-        const fileName = req.file.filename;
+        const fileName = req.file.filename || null;
         let path = fileName ? fileName : null;
-        const type = file.mimetype.startsWith("image/") ? "image" : "video";
 
         const blogPost = new Blog({ content, author: userId, mediaPath: path, mediaType: type
         });
